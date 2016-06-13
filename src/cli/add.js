d233f64403083a145adc3f08b5be445be6d6955e
dutzi <dutzi.b@gmail.com>
2016-06-13 11:29:08 +0300

    Added .watchmanconfig creation

--- src/cli/add.js
+++ src/cli/add.js
@@ -1,13 +1,58 @@
 'use strict';
-var links = require('../links.js');
+
+var fs = require('fs-extra');
 var path = require('path');
 var untildify = require('untildify');
+var inquirer = require('inquirer');
+var isThere = require('is-there');
+var links = require('../links.js');
 
 exports.command = 'add <src> <dest>';
 
 exports.describe = 'Adds a link';
 
 exports.builder = {};
+
+function promptForIgnoredFolders(src, rules) {
+	var prompts = [];
+
+	rules.forEach((rule) => {
+		if (isThere(path.resolve(src, rule.folder))) {
+			prompts.push({
+				name: rule.name,
+				message: rule.message,
+				default: rule.default,
+				type: 'confirm'
+			});
+		}
+	});
+
+	return inquirer.prompt(prompts).then((answers) => {
+		var ignoredFolders = [];
+
+		rules.forEach((rule) => {
+			if (answers[rule.name]) {
+				ignoredFolders.push(rule.folder);
+			}
+		});
+
+		return ignoredFolders;
+	});
+}
+
+function dedupeArray(array) {
+    var arr = array.concat();
+
+    for (var i = 0; i < arr.length; i++) {
+        for(var j = i + 1; j < arr.length; j++) {
+            if (arr[i] === arr[j]) {
+                arr.splice(j--, 1);
+            }
+        }
+    }
+
+    return arr;
+}
 
 exports.handler = function (argv) {
 	links.load();
@@ -16,22 +61,52 @@ exports.handler = function (argv) {
 	    dest = path.resolve(untildify(argv.dest));
 
 	for (i in links.data) {
-		if (links.data[i].src === src) {
-			console.log('Error: a link with the same source already exists');
+		if (links.data[i].src === src &&
+		    links.data[i].dest === dest) {
+			console.log('Error: link already exists');
 			return;
 		}
 	}
 
-	i = 0;
-	while (links.data[i]) i++;
+	promptForIgnoredFolders(src, [{
+		name: 'git',
+		folder: '.git',
+		message: 'Source folder is a git repo, add `.git` to ignored folders?',
+		default: true
+	}, {
+		name: 'npm',
+		folder: 'node_modules',
+		message: 'Source folder is an npm package, add `node_modules` to ignored folders?',
+		default: true
+	}]).then((ignoredFolders) => {
+		var watchmanConfigPath = path.resolve(src, '.watchmanconfig');
 
-	links.data[i] = {
-		src: src,
-		dest: dest,
-		enabled: true,
-		createdTime: new Date()
-	};
+		var watchmanConfig = (() => {
+			try {
+				return fs.readJsonSync(watchmanConfigPath);
+			} catch (err) {
+				return {
+					ignore_dirs: []
+				};
+			}
+		})();
 
-	links.save();
-	console.log(`Added link: (${i}) ${src} -> ${dest}`);
+		ignoredFolders = ignoredFolders.concat(watchmanConfig.ignore_dirs);
+		watchmanConfig.ignore_dirs = dedupeArray(ignoredFolders);
+
+		fs.outputJsonSync(watchmanConfigPath, watchmanConfig);
+
+		i = 0;
+		while (links.data[i]) i++;
+
+		links.data[i] = {
+			src: src,
+			dest: dest,
+			enabled: true,
+			createdTime: new Date()
+		};
+
+		links.save();
+		console.log(`Added link: (${i}) ${src} -> ${dest}`);
+	});
 }
