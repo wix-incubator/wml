4a2d5bbfa2c6ebea0d04d45ca32b72a8436804f5
Ben Allfree <ben@benallfree.com>
2017-11-24 01:00:02 -0800

    Revert watch-project

--- src/watchProject.js
+++ src/watchProject.js
@@ -5,18 +5,11 @@ var Q = require('q');
 module.exports = function (params) {
 	var deferred = Q.defer();
 
-	params.client.command(['watch-del', params.src], (error, resp) => {
+	params.client.command(['watch-project', params.src], (error, resp) => {
 		if (error) {
 			deferred.reject(error);
 		} else {
-			params.client.command(['watch-project', params.src], (error, resp) => {
-				if (error) {
-					deferred.reject(error);
-				} else {
-					deferred.resolve(resp);
-				}
-		
-			});
+			deferred.resolve(resp);
 		}
 
 	});
