0b808fbecdd00afa389750d3a339999f0b7308d9
NathanHaim <haim.nathan@icloud.com>
2018-11-25 18:03:54 +0100

    watchman del before start wml. Useful if .watchmanconfig has changed

--- src/watchProject.js
+++ src/watchProject.js
@@ -5,14 +5,16 @@ var Q = require('q');
 module.exports = function (params) {
 	var deferred = Q.defer();
 
-	params.client.command(['watch-project', params.src], (error, resp) => {
-		if (error) {
-			deferred.reject(error);
-		} else {
-			deferred.resolve(resp);
-		}
-
-	});
+	params.client.command(['watch-del', params.src], () =>
+		params.client.command(['watch-project', params.src], (error, resp) => {
+			console.log(`watching ${params.src}`);
+			if (error) {
+				deferred.reject(error);
+			} else {
+				deferred.resolve(resp);
+			}
+		}),
+		error => deferred.reject(error));
 
 	return deferred.promise;
 }
